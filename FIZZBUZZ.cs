71093e8446c85cdf68efb771d4e11d4cbacac63f
Maciej Owsiejko <kocur31123@wp.pl>
2026-08-20 11:35:40 +0200

    Add project files.

---
+++ FIZZBUZZ.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Generate(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else
+                return number.ToString();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            while (true)
+            {
+                Console.Write("Podaj liczbę (lub wpisz 'exit' aby zakończyć): ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    Console.WriteLine("Koniec gry!");
+                    break;
+                }
+
+                if (int.TryParse(input, out int number))
+                {
+                    string result = fizzBuzz.Generate(number);
+                    Console.WriteLine($"Wynik: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("To nie jest poprawna liczba!");
+                }
+
+                Console.WriteLine(); // pusty wiersz dla czytelności
+            }
+        }
+    }
+}
